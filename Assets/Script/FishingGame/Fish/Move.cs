1e003abadb968407c080980fb43beec2b2745415
HtetAungShine-NineComplex <hasdev.mm@gmail.com>
2024-01-26 07:49:25 +0700

    update move script

--- Assets/Script/FishingGame/Fish/Move.cs
+++ Assets/Script/FishingGame/Fish/Move.cs
@@ -1,54 +1,73 @@
 using UnityEngine;
+using System.Collections;
+
 public class Move : MonoBehaviour
 {
     [SerializeField] private FishSO fishSO;
-    
     private float speed;
     private AnimationCurve curve;
-
-    float elapsedTime;
-    float desiredDuration = 7f;
-
+    private float elapsedTime;
+    private float desiredDuration = 7f;
     private Vector3 _startPoint;
     private Vector3 _endPoint;
-
-    [SerializeField] private FishHealth _health;
+    private FishHealth _health;
 
     private void Start()
     {
         speed = fishSO.speed;
         curve = fishSO.SpeedCurve;
+
+        // Start the coroutine to update the endpoint every 2 seconds
+        StartCoroutine(ChangeEndpointPeriodically(2f));
     }
+
     private void Update()
     {
-       if(_startPoint != null && _endPoint != null)
+        if (_startPoint != null && _endPoint != null)
         {
             MoveFish(_startPoint, _endPoint);
         }
     }
 
-    public void SetPoints(Vector3 startPoint_T, Vector3 destoryPoint_T)
+    public void SetPoints(Vector3 startPoint_T, Vector3 destroyPoint_T)
     {
         _startPoint = startPoint_T;
-        _endPoint = destoryPoint_T;
+        _endPoint = destroyPoint_T;
     }
-    
-    public void MoveFish(Vector3 startPoint, Vector3 destoryPoint)
+
+    public Vector3 GetEndPoint()
     {
-        if(_health != null && _health._isDead)
+        return _endPoint;
+    }
+
+    private void MoveFish(Vector3 startPoint, Vector3 destroyPoint)
+    {
+        if (_health != null && _health._isDead)
         {
             return;
         }
 
         elapsedTime += Time.deltaTime;
         float completePercent = elapsedTime / desiredDuration;
-        transform.position = Vector2.Lerp(startPoint, destoryPoint, curve.Evaluate(completePercent * speed));
-        //transform.position = Vector2.MoveTowards(transform.position, destoryPoint_T.position, speed * Time.deltaTime);
-        transform.up = (destoryPoint - transform.position).normalized;
+        transform.position = Vector2.Lerp(startPoint, destroyPoint, curve.Evaluate(completePercent * speed));
+        transform.up = (destroyPoint - transform.position).normalized;
 
-        float distance = Vector3.Distance(transform.position, destoryPoint);
+        float distance = Vector3.Distance(transform.position, destroyPoint);
 
         if (distance <= 0.1f)
+        {
             Destroy(this.gameObject);
+        }
+    }
+
+    private IEnumerator ChangeEndpointPeriodically(float interval)
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(interval);
+
+            // Update the endpoint periodically
+            _endPoint = SpawnpointManager.Instance.GetRandomEndPoint(_startPoint);
+        }
     }
 }

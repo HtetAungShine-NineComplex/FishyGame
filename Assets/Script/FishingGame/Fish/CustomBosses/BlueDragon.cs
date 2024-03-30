d8625383462b2c7d8a980ab19cbbd099314ff605
Thet Htue Zaw <thetthet311255788@gmail.com>
2024-03-30 15:50:33 +0630

    Fixed_CountImage

--- Assets/Script/FishingGame/Fish/CustomBosses/BlueDragon.cs
+++ Assets/Script/FishingGame/Fish/CustomBosses/BlueDragon.cs
@@ -12,8 +12,10 @@ public class BlueDragon : Fish
 
     [SerializeField] private Image fireBallImg;
     [SerializeField] private GameObject ShadowGO;
-    [SerializeField] private Text MultipleTxt;
-       
+
+    [SerializeField] private Image CountImg;
+    [SerializeField] private List<Sprite> MultiplyCount2Ds = new List<Sprite>();
+
     private enum DeathState
     {
         DyingPhaseOne,
@@ -54,7 +56,7 @@ public class BlueDragon : Fish
         }
     }
 
-    private IEnumerator DyingPhaseOneCoroutine() // Need to apply multiply text
+    /*private IEnumerator DyingPhaseOneCoroutine() // Need to apply multiply text
     {
         ShadowGO.gameObject.SetActive(false);
         fish_2D.enabled = false;
@@ -106,6 +108,69 @@ public class BlueDragon : Fish
         fireBallImg.gameObject.SetActive(false);
         //multiply Text
 
+    }*/
+
+    private IEnumerator DyingPhaseOneCoroutine() // Need to apply multiply text
+    {
+        ShadowGO.gameObject.SetActive(false);
+        fish_2D.enabled = false;
+        fireBallImg.gameObject.SetActive(true);
+        yield return new WaitForSeconds(1.2f);
+
+        CountImg.gameObject.SetActive(true);
+        CountImg.transform.rotation = new Quaternion(0, 0, 0, 1);
+
+        CountImg.sprite = MultiplyCount2Ds[0];
+        SmallDragonManager.Instance.SpawnSmallDragon_Left();
+        SmallDragonManager.Instance.SpawnSmallDragon_Right();
+        FishesInstantDie();
+        yield return new WaitForSeconds(2.2f);
+
+        CountImg.sprite = MultiplyCount2Ds[1];
+        SmallDragonManager.Instance.SpawnSecSmallDragon_Left();
+        SmallDragonManager.Instance.SpawnSecSmallDragon_Right();
+        FishesInstantDie();
+        yield return new WaitForSeconds(2.1f);
+
+        CountImg.sprite = MultiplyCount2Ds[2];
+        SmallDragonManager.Instance.SpawnSecSmallDragon_Top();
+        SmallDragonManager.Instance.SpawnSmallDragon_Bot();
+        FishesInstantDie();
+        yield return new WaitForSeconds(1.9f);
+
+        CountImg.sprite = MultiplyCount2Ds[3];
+        SmallDragonManager.Instance.SpawnSecSmallDragon_Left();
+        SmallDragonManager.Instance.SpawnSecSmallDragon_Right();
+        SmallDragonManager.Instance.SpawnSecSmallDragon_Top();
+        SmallDragonManager.Instance.SpawnSecSmallDragon_Bot();
+        FishesInstantDie();
+        yield return new WaitForSeconds(1.8f);
+
+        CountImg.sprite = MultiplyCount2Ds[4];
+        ExplosionEffectManager.Instance.ShowExplosionEffects();
+        SmallDragonManager.Instance.SpawnSmallDragon_Left();
+        SmallDragonManager.Instance.SpawnSmallDragon_Right();
+        SmallDragonManager.Instance.SpawnSecSmallDragon_Top();
+        SmallDragonManager.Instance.SpawnSmallDragon_Bot();
+        FishesInstantDie();
+
+        yield return new WaitForSeconds(1.7f);
+        
+
+        CountImg.gameObject.SetActive(false);
+        fireBallImg.gameObject.SetActive(false);
+        //multiply Text
+
+    }
+
+    private static void FishesInstantDie()
+    {
+        List<FishHealth> fishList = GeneratedFishManager.Instance.GetGeneratedFishList();
+
+        for (int a = 0; a < fishList.Count; a++)
+        {
+            fishList[a].InstantDie();
+        }
     }
 
     private void LerpToMiddle()

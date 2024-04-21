12257b14884dfc5e79cfec72e7cdbd616999e588
Thet Htue Zaw <thetthet311255788@gmail.com>
2024-04-21 23:43:14 +0630

    Fixed_FishSize_&_Added_FlyFish

--- Assets/Script/MainUIs/UIRoomSelection.cs
+++ Assets/Script/MainUIs/UIRoomSelection.cs
@@ -9,6 +9,8 @@ public class UIRoomSelection : UIBase
     [SerializeField] private Button _backBtn;
     [SerializeField] private Button _selectChairBtn; //temporary use only
 
+    private bool _sceneChanged = false;
+
     protected override void OnShow(UIBaseData Data = null)
     {
         base.OnShow(Data);
@@ -26,18 +28,23 @@ public class UIRoomSelection : UIBase
 
     private void BackToFishingGameUI()
     {
+        _sceneChanged = false;
         UIManager.Instance.ShowUI(GLOBALCONST.UI_FISHING_GAME);
         UIManager.Instance.CloseUI(GLOBALCONST.UI_ROOM_SELECT);
     }
 
     private void EnterRoom() //temp
     {
+        _sceneChanged = true;
         UIManager.Instance.ShowUI(GLOBALCONST.UI_LOADING);
         SceneLoader.Instance.LoadSceneAsync((int)SceneIndex.FISHING_GAME_SCENE, false);
     }
 
     private void OnDisable()
     {
-        UIManager.Instance.CloseAllOpeningUIs();
+        if(_sceneChanged)
+        {
+            UIManager.Instance.CloseAllOpeningUIs();
+        }
     }
 }

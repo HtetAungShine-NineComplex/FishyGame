f24db07e44452ef535bac0eafe919505ab78e96b
LAPTOP-VOT11GKE\Shine <hasdev.mm@gmail.com>
2024-04-22 00:21:22 +0700

    Added Splash Screen Background

--- Assets/Script/UIProps/GameUIButton.cs
+++ Assets/Script/UIProps/GameUIButton.cs
@@ -13,6 +13,8 @@ public class GameUIButton : MonoBehaviour
 {
     [SerializeField] private Button _btn;
     [SerializeField] private GameType _gameType; //the game type that will be loaded on click
+    [SerializeField] private Image m_SplashScreen;
+    [SerializeField] private Sprite m_SplashScreenArt;
 
     private void OnEnable()
     {
@@ -26,6 +28,15 @@ public class GameUIButton : MonoBehaviour
 
     private void GoToGameUI()
     {
+        StartCoroutine(IGoTOGameUI());
+    }
+
+    IEnumerator IGoTOGameUI()
+    {
+        m_SplashScreen.gameObject.SetActive(true);
+        m_SplashScreen.sprite = m_SplashScreenArt;
+        yield return new WaitForSeconds(1f);
+        m_SplashScreen.gameObject.SetActive(false);
         UIManager.Instance.CloseUI(GLOBALCONST.UI_MAIN_MENU);
         UIManager.Instance.ShowUI(GLOBALCONST.UI_FISHING_GAME);
     }

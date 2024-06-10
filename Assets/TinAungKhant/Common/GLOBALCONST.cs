6b4e2c440decc552a4e82d276fcd1fee544d0241
LAPTOP-VOT11GKE\Shine <hasdev.mm@gmail.com>
2024-06-10 10:57:21 +0630

    Added New Fishing Game - LuKui

--- Assets/TinAungKhant/Common/GLOBALCONST.cs
+++ Assets/TinAungKhant/Common/GLOBALCONST.cs
@@ -10,10 +10,16 @@ public class GLOBALCONST
 	public const string LOADING_OBJECT = "LoadingObject";
 	public const string UI_MAIN_MENU = "UI_MainMenu";
 	public const string UI_FISHING_GAME = "UI_FishingGame";
+    public const string UI_FISHING_GAME_OCTOPUS = "UI_FishingGame_Octopus"; 
+    public const string UI_FISHING_GAME_BUFFALO = "UI_FishingGame_Buffalo";
+    public const string UI_FISHING_GAME_LIKUI = "UI_FishingGame_LiKui";
     public const string UI_FISHING_GAME_KIRI = "UI_FishingGame_Kiri";
     public const string UI_LOADING = "UI_Loading";
 	public const string UI_ROOM_SELECT = "UI_RoomSelection";
+    public const string UI_ROOM_SELECT_OCTOPUS = "UI_RoomSelection_Octopus";
     public const string UI_ROOM_SELECT_KIRI = "UI_RoomSelection_Kiri";
+    public const string UI_ROOM_SELECT_BUFFALO = "UI_RoomSelection_Buffalo";
+    public const string UI_ROOM_SELECT_LIKUI = "UI_RoomSelection_LiKui";
     public const string UI_FISHSTATS = "UI_InGameFishStats";
 	public const string UI_INGAMEQUIT = "UI_InGameQuit";
     public const string UI_INGAMESETTING = "UI_InGameSetting";

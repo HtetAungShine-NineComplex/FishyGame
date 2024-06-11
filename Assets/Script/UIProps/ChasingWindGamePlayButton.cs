cb32cb164ac185c17319f804597623c2798c1524
LAPTOP-VOT11GKE\Shine <hasdev.mm@gmail.com>
2024-06-11 17:22:28 +0630

    Added New 14 Fishs for Chasing Wind Fishing Game

---
+++ Assets/Script/UIProps/ChasingWindGamePlayButton.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using TinAungKhant.UIManagement;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChasingWindGamePlayButton : MonoBehaviour
+{
+    [SerializeField] private Button _btn;
+
+    private void OnEnable()
+    {
+        _btn.onClick.AddListener(GoToGameplay);
+    }
+
+    private void GoToGameplay()
+    {
+        UIManager.Instance.ShowUI(GLOBALCONST.UI_ROOM_SELECT_CHASINGWIND);
+        UIManager.Instance.CloseUI(GLOBALCONST.UI_FISHING_GAME_CHASINGWIND);
+    }
+}
---
+++ Assets/Script/UIProps/ChasingWindGameUIButton.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using TinAungKhant.UIManagement;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChasingWindGameUIButton : MonoBehaviour
+{
+    [SerializeField] private Button _btn;
+    [SerializeField] private GameType _gameType; //the game type that will be loaded on click
+    [SerializeField] private Image m_SplashScreen;
+    [SerializeField] private Sprite m_SplashScreenArt;
+    [SerializeField] private float fadeDuration = 0.75f;
+
+    private void OnEnable()
+    {
+        _btn.onClick.AddListener(GoToGameUI);
+    }
+
+    private void OnDisable()
+    {
+        _btn.onClick.RemoveAllListeners();
+    }
+
+    private void GoToGameUI()
+    {
+        StartCoroutine(IGoTOGameUI());
+    }
+
+    IEnumerator IGoTOGameUI()
+    {
+        m_SplashScreen.gameObject.SetActive(true);
+        m_SplashScreen.sprite = m_SplashScreenArt;
+        yield return new WaitForSeconds(fadeDuration);
+        m_SplashScreen.gameObject.SetActive(false);
+        UIManager.Instance.CloseUI(GLOBALCONST.UI_MAIN_MENU);
+        UIManager.Instance.ShowUI(GLOBALCONST.UI_FISHING_GAME_CHASINGWIND);
+    }
+}
